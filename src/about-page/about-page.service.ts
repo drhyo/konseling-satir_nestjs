94c73e77bbd4108cb3c3a7cd1755292f4ef90590
yudha <yudajaya75@gmail.com>
2023-08-15 17:12:49 +0700

    update

--- src/about-page/about-page.service.ts
+++ src/about-page/about-page.service.ts
@@ -8,29 +8,29 @@ import 'dotenv/config'
 @Injectable()
 export class AboutPageService {
     constructor(
-       @InjectRepository(AboutPageEntity)
-       private aboutPageRepository: Repository<AboutPageEntity> 
-    ){ }
+        @InjectRepository(AboutPageEntity)
+        private aboutPageRepository: Repository<AboutPageEntity>
+    ) { }
 
     async findData(id: number) {
-        return await this.aboutPageRepository.findOne({where:{id}})
+        return await this.aboutPageRepository.findOne({ where: { id } })
     }
 
-    async getImage (): Promise<any> {
+    async getImage(): Promise<any> {
         try {
-            const res = await axios.get( process.env.STRAPI_URL + 'about?populate=*')
+            const res = await axios.get(process.env.STRAPI_URL + 'about?populate=*')
             const findImage = res.data.data.attributes.image.data.attributes.url;
-            const imageUrl = `http://localhoast:1337${ findImage }`
-            return{
-                    url: imageUrl
+            const imageUrl = `http://localhost:1337${findImage}`
+            return {
+                url: imageUrl
             }
         } catch (error) {
             console.log(error);
             throw new NotFoundException('failed')
         }
     }
-    
-    async findDataWithImage (id: number): Promise<AboutPageEntity> {
+
+    async findDataWithImage(id: number): Promise<AboutPageEntity> {
         const findData = await this.findData(id)
         const getImage = await this.getImage()
 
@@ -40,8 +40,8 @@ export class AboutPageService {
                 ...getImage
             }
         }
-       const data = merge();
-       return data;
+        const data = merge();
+        return data;
 
 
 
